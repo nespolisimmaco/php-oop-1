f99ea8e68cd62e89b7f4f8f10f4b607360560ec4
nespolisimmaco <snespoli@yahoo.com>
2023-05-16 18:44:19 +0200

    Add: created a class Movie and three 'Movie' objects, print their properties with 'printdata()'

--- Models/Movie.php
+++ Models/Movie.php
@@ -2,4 +2,25 @@
 
 class Movie
 {
+    public $title;
+    public $genre;
+    public $year;
+    public $director;
+
+    function __construct($_title, $_genre)
+    {
+        $this->title = $_title;
+        $this->genre = $_genre;
+    }
+
+    public function setData($_year, $_director)
+    {
+        $this->year = $_year;
+        $this->director = $_director;
+    }
+
+    public function printData()
+    {
+        return "Titolo del film: {$this->title} - Genere/i: {$this->genre} - Anno: {$this->year} - Regista: {$this->director}";
+    }
 }
--- index.php
+++ index.php
@@ -1,3 +1,22 @@
 <?php
 
 require_once __DIR__ . "/Models/Movie.php";
+
+$first_movie = new Movie("Interstellar", "Fantascienza");
+$second_movie = new Movie("Forrest Gump", "Comedy-drama");
+$third_movie = new Movie("Titanic", "Romance");
+
+var_dump($first_movie);
+var_dump($second_movie);
+var_dump($third_movie);
+
+$first_movie->setData("2014", "Christopher Nolan");
+$second_movie->setData("1994", "Robert Zemeckis");
+$third_movie->setData("1997", "James Cameron");
+
+echo ($first_movie->printData());
+echo ("<br>");
+echo ($second_movie->printData());
+echo ("<br>");
+echo ($third_movie->printData());
+echo ("<br>");
